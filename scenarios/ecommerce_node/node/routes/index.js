e8956f259ac3cfe1e5c4f0ddcbfbb290d28a8022
Zhongjun Jin <markjin1990@markideapad.(none)>
2014-02-23 00:09:31 -0500

    In the process of implementing CreateAB feature. Not complete

--- scenarios/ecommerce_node/node/routes/index.js
+++ scenarios/ecommerce_node/node/routes/index.js
@@ -26,5 +26,7 @@ exports.checkuser = function(req, res){
 	// TODO: If the user has been found
 
 	// TODO: If the user is not found
-	res.render('test', { retStr: 'check user'});
+	var states = [ "AA","AE","AP","AL","AK","AS","AZ","AR","CA","CO","CT","DE","DC","FM","FL","GA","GU","HI","ID","IL","IN","IA","KS","KY","LA","ME","MH","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","MP","OH","OK","OR","PW","PA","PR","RI","SC","SD","TN","TX","UT","VT","VI","VA","WA","WV","WI","WY"
+	];
+	res.render('createAB', {states:states });
 }
