4f2704b5b4f09fd185ef86b2f5aa11bc52441cb3
Zhongjun Jin <markjin1990@markideapad.(none)>
2014-03-08 13:13:25 -0500

    Inserted shop button in catalog. Will create a shoplist page later today.

--- abrest/public/javascripts/browse.js
+++ abrest/public/javascripts/browse.js
@@ -3,10 +3,21 @@ $(document).ready(function(){
 		$(this).animate({height:"+=30px"});
 	});
 	*/
+	$("[class=shopbtn]").hide();
 	$(".hoverDiv").mouseenter(function(){
 		$(this).animate({height:"+=50px"});
+		var id = $(this).attr('id');
+		id = id.substr(4);
+		 var fix = '#shopbtn_' + id;
+		$("#shopbtn_").val("'"+fix+"'");
+		$(fix).show("slow");
 	});
 	$(".hoverDiv").mouseleave(function(){
 		$(this).animate({height:"-=50px"});
+		var id = $(this).attr('id');
+		id = id.substr(4);
+		 var fix = '#shopbtn_' + id;
+		$("#shopbtn_").val("'"+fix+"'");
+		$(fix).hide();
 	});
 });
--- abrest/routes/index.js
+++ abrest/routes/index.js
@@ -1,4 +1,8 @@
 
 exports.index = function(req, res){
-	res.render('index', { title: 'E-Commerce' });
+	// session info
+	var login = req.session.login;
+	var user = req.session.user;
+
+	res.render('index', { title: 'E-Commerce', login:login, user:user});
 };
