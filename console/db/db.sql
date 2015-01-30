75f2dc4b652d5d272129b87f3831f7fed11282b9
dulybysh <talkadmin@ubuntu.(none)>
2015-01-29 20:17:26 -0500

    1. Dropping tables Service_Data, Service_Log is now done before dropping table Service. Otherwise, when executing CREATE TABLE Service  we will get the following error: ERROR 1050 (42S01): Table 'Service' already exists
    and when executing INSERT INTO Service(...) we would get the following error:
    ERROR 1062 (23000): Duplicate entry '1' for key 'PRIMARY'
    
    2. Table Service_Data is created and data is inserted into it.
    service_id is the foreign key referenced from  Service.id

--- console/db/db.sql
+++ console/db/db.sql
@@ -2,20 +2,23 @@ CREATE DATABASE IF NOT EXISTS abconsole;
 
 USE abconsole;
 
+DROP TABLE IF EXISTS Service_Log;
+DROP TABLE IF EXISTS Service_Data;
+
 DROP TABLE IF EXISTS Service;
 CREATE TABLE Service (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(2048),
-	display_name VARCHAR(2048),
-	rating FLOAT,
-	trust_level FLOAT,
-	req_data VARCHAR(2048),
-	host VARCHAR(2048),
-	port INT,
-	url VARCHAR(2048),
-	params TEXT,
-	status INT,
-	source_path VARCHAR(2048)
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(2048),
+        display_name VARCHAR(2048),
+        rating FLOAT,
+        trust_level FLOAT,
+        req_data VARCHAR(2048),
+        host VARCHAR(2048),
+        port INT,
+        url VARCHAR(2048),
+        params TEXT,
+        status INT,
+        source_path VARCHAR(2048)
 ) ENGINE=InnoDB;
 
 INSERT INTO Service(id, name, display_name, rating, trust_level, req_data, host, port, url, params, status, source_path) VALUES
@@ -24,12 +27,11 @@ INSERT INTO Service(id, name, display_name, rating, trust_level, req_data, host,
 (3, 'localhost:4103','Shipping', 5.0, 4.0, 'address', 'localhost', 4103, 'http://localhost:4103', '{}', -1, 'scenarios/online-shopping/shipping'),
 (4, 'localhost:4104','Payment', 5.0, 4.0, 'credit card', 'localhost', 4104, 'http://localhost:4104', '{}', -1, 'scenarios/online-shopping/payment');
 
-DROP TABLE IF EXISTS Service_Log;
 CREATE TABLE Service_Log (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	service_id INT,
-	log VARCHAR(2048),
-	FOREIGN KEY (service_id) REFERENCES Service(id)
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        service_id INT,
+        log VARCHAR(2048),
+        FOREIGN KEY (service_id) REFERENCES Service(id)
 ) ENGINE=InnoDB;
 
 INSERT INTO Service_Log(id, service_id, log) VALUES
@@ -37,3 +39,19 @@ INSERT INTO Service_Log(id, service_id, log) VALUES
 (2, 2, 'AB Data: expedited shipping'),
 (3, 3, 'AB Data: 305 N Univ St West Lafayette IN'),
 (4, 4, 'AB Data: 1234 5678 9012 3456');
+
+CREATE TABLE Service_Data (
+        id INT PRIMARY KEY,
+        service_id INT ,
+        data_key VARCHAR(1024) ,
+        data_value VARCHAR(1024), 
+        FOREIGN KEY (service_id) REFERENCES Service(id) 
+)ENGINE=INNODB ;
+
+INSERT INTO Service_Data(id , service_id, data_key, data_value) VALUES
+(1, 1, 'name','John'),
+(2, 2, 'email','john@purdue.com'),
+(3, 4, 'creditcard','1111 2222 3333 4444'),
+(4, 1, 'payment.type','visa'),
+(5, 3, 'address','305 N University St, West Lafayette, IN, US'), 
+(6, 2, 'shipping.preference','expedited shipping');
