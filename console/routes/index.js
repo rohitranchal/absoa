20cd76ea99c369ceca816616342f7b9616457928
dulybysh <talkadmin@ubuntu.(none)>
2015-02-12 16:36:56 -0500

    JSON object is created in router.get('/client', ... ) method for 'environment' column of the table 'Policy'. JSON object was tested for the first tuple of 'Policy' table var obj = JSON.parse(policies[0].environment);

--- console/routes/index.js
+++ console/routes/index.js
@@ -46,6 +46,9 @@ router.get('/client', function(req, res) {
 		console.log('rows: ' + rows[0].data_key + ' = ' + rows[0].data_value);
 		db.get_policy(function(policies) {
 			console.log('policies: ' + policies[0].id + ' ; ' + policies[0].policy + ' ; ' + policies[0].environment);
+			var obj = JSON.parse(policies[0].environment);
+			console.log('obj: ' + obj);
+			console.log('obj data: ' + obj.rating + ' ; credit limit=' + obj.credit_limit);
 			res.render('client', { title: 'Active Bundle Console', entries: rows, entries_pol: policies });
 		});	
 	});
